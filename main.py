921d5b16d85170c0fdf57a1d8788ea908e8f5d1e
Evan Voeltner <evanvoeltner@gmail.com>
2022-08-16 13:24:15 -0500

    Filled out some of the lists and added a function to return a random element. Started on the main function (probably should have broken this up more, but I'm new to git so I'll give my self a pass)

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-# Day Trip Generator project
-# I need something to change for a sec
-# I need to make sure it is the right github account
+import random
+
+list_of_destinations = ['Milwaukee', 'West Allis', 'Wisconsin Dells', 'Chicago', ]
+list_of_transportations = ['Bus', 'Airplane', 'Rental Car', 'Boat', 'Train', 'Blimp']
+list_of_restraunts = []
+list_of_entertainment = []
+final_trip = {
+    'location': '',
+    'transportation' : '',
+    'restraunt': '',
+    'entertainment': ''
+}
+
+
+def select_random(given_list):
+    selected_item = random.sample(given_list, k=1)
+    return(selected_item.pop())
+
+
+
+def confirm_selection():
+    location_selection = ''
+    transportation_selection = ''
+    restraunt_selection = ''
+    entertainment_selection = ''
+
+    
+
+    
+    print('We will plan your trip for you!')
+
+
+
+# print(select_random(list_of_destinations))
